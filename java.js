2c86d917f9310f1dcb9068445544fa53f4e20237
Thomas Kwapis <ThomasKwapis@outlook.de>
2022-03-31 18:59:29 +0200

    update

--- java.js
+++ java.js
@@ -17,7 +17,6 @@ const config = {
     trustServerCertificate: true
   }
 };
-
 const connection = new Connection(config);
 
 // Attempt to connect and execute queries if connection goes through
@@ -36,7 +35,10 @@ function queryDatabase() {
 
   // Read all rows from table
   const request = new Request(
-    `SELECT TOP 20 Name from SalesLT.product`,
+    `SELECT TOP 20 pc.Name as CategoryName,
+                   p.name as ProductName
+     FROM [SalesLT].[ProductCategory] pc
+     JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid`,
     (err, rowCount) => {
       if (err) {
         console.error(err.message);
